bc9991ee9ec4af6dd18550f92365b53ed02e80f6
akbarjohnie <akbar.rashidov.2013@mail.ru>
2022-08-18 13:22:27 +0300

    done with current weather

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatefulWidget {
-  const FirstPage({Key? key}) : super(key: key);
+  const FirstPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<FirstPage> createState() => _FirstPageState();
@@ -19,6 +21,10 @@ class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather app'),
+        centerTitle: true,
+      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:test_task_friflex/model/weather_model.dart';
+import 'package:test_task_friflex/services/weather_api_client.dart';
+import 'package:test_task_friflex/widget/additional_info.dart';
+import 'package:test_task_friflex/widget/current_weather.dart';
 
 class SecondPage extends StatefulWidget {
   const SecondPage({Key? key}) : super(key: key);
@@ -8,6 +12,19 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  WeatherApiClient client = WeatherApiClient();
+  Weather? data;
+
+  Future<void> getData() async {
+    data = await client.getCurrentWeather('Moscow');
+  }
+
+  // @override
+  // void initState() {
+  //   client.getCurrentWeather('Moscow');
+  //   super.initState();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +57,55 @@ class _SecondPageState extends State<SecondPage> {
           )
         ],
       ),
-      body: Column(children: []),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              children: [
+                currentWeather(
+                  Icons.cloud_sharp,
+                  '${data!.temperature}',
+                  '${data!.cityName}',
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Additional information',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 20,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Divider(color: Colors.grey, thickness: 1),
+                ),
+                additionalInfo(
+                  data!.temperature.toString(),
+                  data!.wind.toString(),
+                  data!.humidity.toString(),
+                  data!.pressure.toString(),
+                )
+              ],
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Something went wrong',
+                ),
+                CircularProgressIndicator()
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
